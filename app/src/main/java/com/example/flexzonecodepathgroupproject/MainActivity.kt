e0b04c3bac3f446bd4825a1ee9f563ea5ee17909
bcaatmann <aatmannred@gmail.com>
2022-10-30 15:36:25 -0400

    git commit 'stream_page'

--- app/src/main/java/com/example/flexzonecodepathgroupproject/MainActivity.kt
+++ app/src/main/java/com/example/flexzonecodepathgroupproject/MainActivity.kt
@@ -8,7 +8,11 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.group.myapplication.CustomAdapter
+import com.group.myapplication.ItemsViewModel
 import com.parse.ParseUser
 
 class MainActivity : AppCompatActivity() {
@@ -59,3 +63,32 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 }
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        // getting the recyclerview by its id
+//        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+//
+//        // this creates a vertical layout Manager
+//        recyclerview.layoutManager = LinearLayoutManager(this)
+//
+//        // ArrayList of class ItemsViewModel
+//        val data = ArrayList<ItemsViewModel>()
+//
+//        // This loop will create 20 Views containing
+//        // the image with the count of view
+//        for (i in 1..20) {
+//            data.add(ItemsViewModel(R.drawable.ic_launcher_background, "Muscle " + i))
+//        }
+//
+//        // This will pass the ArrayList to our Adapter
+//        val adapter = CustomAdapter(data)
+//
+//        // Setting the Adapter with the recyclerview
+//        recyclerview.adapter = adapter
+//
+//    }
+//}
